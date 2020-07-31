168fa412ba1fa14c0f83a8a8c35b5b11569d4e65
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2020-07-31 16:00:05 +0500

    update added shoping-edit component

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.scss']
+})
+export class ShoppingEditComponent implements OnInit {
+
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+
+  @Output() addedIngredient = new EventEmitter<{ name: string, amount: number }>();
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredientAdded = new Ingredient(ingName, ingAmount);
+    this.addedIngredient.emit(newIngredientAdded);
+  }
+
+}
