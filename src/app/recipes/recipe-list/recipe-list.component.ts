35eb3e749170fc4f429e44fefe7e212ac49f6fe3
UsmanHaiderKhan4 <usmanhaiderkhan4@gmail.com>
2020-09-05 23:16:31 +0500

    Update the List Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,23 +1,17 @@
-
-import { Component, Output, EventEmitter } from "@angular/core";
-import { Recipe } from "../recipe.model";
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 import { from } from 'rxjs';
+import { RecipeService } from '../recipe.service';
 
 @Component({
-    selector: 'app-recipe-list',
-    templateUrl: './receipe-list.component.html'
+  selector: 'app-recipe-list',
+  templateUrl: './receipe-list.component.html',
 })
-export class RecipeList {
-    @Output() recipeWasSelected = new EventEmitter<Recipe>();
-    recipes: Recipe[] = [
-        new Recipe('Biryani-1', 'Very tasty Lahori Biryani', 'https://upload.wikimedia.org/wikipedia/commons/3/35/Biryani_Home.jpg'),
-        new Recipe('Biryani-2', 'Very tasty Lahori Biryani', 'https://upload.wikimedia.org/wikipedia/commons/3/35/Biryani_Home.jpg'),
-        new Recipe('Biryani-3', 'Very tasty Lahori Biryani', 'https://upload.wikimedia.org/wikipedia/commons/3/35/Biryani_Home.jpg'),
+export class RecipeList implements OnInit {
+  recipes: Recipe[];
 
-    ];
-    onSelectedRecipe(recipe: Recipe) {
-        this.recipeWasSelected.emit(recipe);
-    }
-
-
+  constructor(private recipeService: RecipeService) {}
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 }
