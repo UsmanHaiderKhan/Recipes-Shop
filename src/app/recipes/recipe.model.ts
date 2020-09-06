ec59fc58ac12363b89d5ff3cd6d02bad778b8734
UsmanHaiderKhan4 <usmanhaiderkhan4@gmail.com>
2020-09-06 23:11:26 +0500

    Update added Recipes Propertys

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,20 @@
-export class Recipe {
-    public name: string;
-    public description: string;
-    public imageUrl: string;
+import { Ingredient } from '../shared/ingredient.model';
 
-    constructor(name: string, description: string, imageUrl: string) {
-        this.name = name;
-        this.description = description;
-        this.imageUrl = imageUrl;
-    }
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imageUrl: string;
+  public ingredient: Ingredient[];
+
+  constructor(
+    name: string,
+    description: string,
+    imageUrl: string,
+    ingredient: Ingredient[]
+  ) {
+    this.name = name;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this.ingredient = ingredient;
+  }
 }
