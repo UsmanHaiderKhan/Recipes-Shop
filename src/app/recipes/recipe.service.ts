9bc059161ef6f3527c0b4c02c3b2bb1edf673d00
UsmanHaiderKhan4 <usmanhaiderkhan4@gmail.com>
2020-09-06 23:12:19 +0500

    Update added new Service to get recipe by id

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  completeRecipe = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe(
+      'Biryani-1',
+      'Very tasty Lahori Biryani',
+      'https://upload.wikimedia.org/wikipedia/commons/3/35/Biryani_Home.jpg',
+      [
+        new Ingredient('Meat', 2),
+        new Ingredient('Rice', 2),
+        new Ingredient('Oil', 1),
+      ]
+    ),
+    new Recipe(
+      'Biryani-2',
+      'Very tasty Lahori Biryani',
+      'https://upload.wikimedia.org/wikipedia/commons/3/35/Biryani_Home.jpg',
+      [
+        new Ingredient('Meat', 2),
+        new Ingredient('Rice', 2),
+        new Ingredient('Oil', 1),
+      ]
+    ),
+    new Recipe(
+      'Biryani-3',
+      'Very tasty Lahori Biryani',
+      'https://upload.wikimedia.org/wikipedia/commons/3/35/Biryani_Home.jpg',
+      [
+        new Ingredient('Meat', 2),
+        new Ingredient('Rice', 2),
+        new Ingredient('Oil', 1),
+      ]
+    ),
+  ];
+  constructor(private shoppingListService: ShoppingListService) {}
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipeById(index: number) {
+    return this.recipes[index];
+  }
+  addIngredientToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+}
