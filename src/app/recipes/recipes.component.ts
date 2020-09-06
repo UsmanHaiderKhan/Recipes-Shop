2659470acf7685c034d8ccbdd8aac8a1e571b06f
UsmanHaiderKhan4 <usmanhaiderkhan4@gmail.com>
2020-09-06 23:13:35 +0500

    Update :just  Refactoring of Code for Routing

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { from } from 'rxjs';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.scss']
+  styleUrls: ['./recipes.component.scss'],
 })
 export class RecipesComponent implements OnInit {
-
-  selectedRecipe: Recipe;
-  constructor() { }
+  completeRecipe: Recipe;
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
+    this.recipeService.completeRecipe.subscribe((recipe: Recipe) => {
+      this.completeRecipe = recipe;
+    });
   }
-
 }
